943ee8dd693a3d64a620177faf1ed37509b2a891
Ala <ala.menai.pro@gmail.com>
2025-02-03 19:19:27 +0100

    ui: add components documentation page

---
+++ app/docs/components/[slug]/page.tsx
@@ -0,0 +1,71 @@
+"use client"
+
+import { allComponents } from "@/.content-collections/generated"
+import { Button } from "@/components/ui/button"
+import { ArrowLeft, ArrowRight } from "lucide-react"
+import { notFound, useParams, useRouter } from "next/navigation"
+
+export default function Page() {
+  const params = useParams()
+  const router = useRouter()
+
+  // Find the component based on the slug from the URL
+  const currentIndex = allComponents.findIndex(
+    (post) => post._meta.path === params.slug
+  )
+  const post = allComponents[currentIndex]
+
+  // Handle case where component is not found
+  if (!post) {
+    return notFound()
+  }
+
+  // Navigate to the next component
+  const goToNext = () => {
+    const nextIndex = currentIndex + 1
+    if (nextIndex < allComponents.length) {
+      router.push(`/docs/components/${allComponents[nextIndex]._meta.path}`)
+    }
+  }
+
+  // Navigate to the previous component
+  const goToPrevious = () => {
+    const prevIndex = currentIndex - 1
+    if (prevIndex >= 0) {
+      router.push(`/docs/components/${allComponents[prevIndex]._meta.path}`)
+    }
+  }
+
+  return (
+    <article>
+      <post.mdx />
+      <div className="w-full flex justify-between mt-5">
+        <div>
+          {currentIndex > 0 && (
+            <Button
+              variant="outline"
+              className="rounded-full"
+              onClick={goToPrevious}
+            >
+              <ArrowLeft />
+              Previous
+            </Button>
+          )}
+        </div>
+
+        <div>
+          {currentIndex < allComponents.length - 1 && (
+            <Button
+              variant="outline"
+              className="rounded-full"
+              onClick={goToNext}
+            >
+              Next
+              <ArrowRight />
+            </Button>
+          )}
+        </div>
+      </div>
+    </article>
+  )
+}
