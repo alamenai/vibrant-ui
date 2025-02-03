fdf4b1b4b2af8dac7601ab51cabf87dc72600ac6
Ala <ala.menai.pro@gmail.com>
2025-02-03 19:19:57 +0100

    ui: add documentation layout

---
+++ app/docs/layout.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "@/layouts/sidebar"
+import { ReactNode } from "react"
+
+const DocLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="flex pl-24 pr-96 min-h-screen gap-20">
+      <Sidebar />
+      <div className="min-w-full">{children}</div>
+    </main>
+  )
+}
+
+export default DocLayout
