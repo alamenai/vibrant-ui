230c8dfbd0beef1ac6d89366e6f4f1f06e95a1bb
Ala <ala.menai.pro@gmail.com>
2025-02-03 19:18:42 +0100

    ui: create the first version of the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,49 @@
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import WavyText from "@/components/wavy-text";
-import Link from "next/link";
+"use client"
+
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { MonthSlider } from "@/components/vibrant/month-slider"
+import { Hero } from "@/layouts/hero"
+import { ArrowRight } from "lucide-react"
+import Link from "next/link"
 
 const Page = () => {
   return (
-    <div className="w-screen h-screen flex flex-col items-center pt-24">
-      <Badge className="bg-rose-500 hover:bg-rose-500">Alpha</Badge>
-      <WavyText text="Vibrant UI" />
-      <p className="text-2xl">Make your user interfaces more vibrant.</p>
-      <Link href="https://github.com/alamenai/vibrant-ui" target="_blank">
-        <Button className="bg-violet-600 hover:bg-violet-700 rounded-full mt-16">
-          See on Github
-        </Button>
-      </Link>
+    <div className="flex flex-col items-center overflow-x-hidden">
+      <Hero />
+      <Card className="rounded-2xl my-8">
+        <CardHeader className="flex-row relative w-full items-center justify-between">
+          <CardTitle>Month Slider</CardTitle>
+          <Badge className="absolute right-4 bg-rose-500 hover:bg-rose-600">
+            New
+          </Badge>
+          <CardDescription></CardDescription>
+        </CardHeader>
+        <CardContent>
+          <MonthSlider />
+        </CardContent>
+        <CardFooter>
+          <Link href="/docs/components/month-slider" className="w-full">
+            <Button
+              variant="outline"
+              className="w-full rounded-full items-center relative bg-slate-50 border-none font-semibold"
+            >
+              Try it now
+              <ArrowRight className="absolute right-3" />
+            </Button>
+          </Link>
+        </CardFooter>
+      </Card>
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
