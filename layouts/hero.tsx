39958a3838b6ec7c2f4e1ca8500057fbd2692727
Ala <ala.menai.pro@gmail.com>
2025-02-03 19:09:48 +0100

    ui: add hero section

---
+++ layouts/hero.tsx
@@ -0,0 +1,32 @@
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import WavyText from "@/components/vibrant/wavy-text"
+import { Github } from "lucide-react"
+import Link from "next/link"
+
+export const Hero = () => {
+  return (
+    <div className="mt-8 flex flex-col items-center">
+      <Badge className="bg-rose-500 hover:bg-rose-500 text-sm">
+        Built for design engineers
+      </Badge>
+      <WavyText text="Create Vibrant User Interfaces" />
+      <p className="text-2xl max-w-2xl text-center">
+        Modern, animated, and ready-to-use components to make your interfaces
+        vibrant and visually stunning.
+      </p>
+      <div className="flex gap-2">
+        <Link href="/docs/components/month-slider">
+          <Button className="bg-violet-600 hover:bg-violet-700 rounded-full mt-8">
+            Browse Components
+          </Button>
+        </Link>
+        <Link href="https://github.com/alamenai/vibrant-ui" target="_blank">
+          <Button variant="outline" className="rounded-full mt-8">
+            <Github /> See on Github
+          </Button>
+        </Link>
+      </div>
+    </div>
+  )
+}
