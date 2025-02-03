a0f876309d2a33bd127677b03a41b0669f446b78
Ala <ala.menai.pro@gmail.com>
2025-02-03 19:07:32 +0100

    config: update Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,46 @@
-import type { NextConfig } from "next";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
+import { withContentCollections } from "@content-collections/next";
+import createMDX from "@next/mdx";
+import { NextConfig } from "next";
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.gravatar.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "gravatar.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [
+      ["remark-gfm" as any, { strict: true, throwOnError: true }],
+      ["remark-math", { strict: true, throwOnError: true }],
+      ["remark-toc", { strict: true, throwOnError: true }],
+      ["remark-mdx", { strict: true, throwOnError: true }],
+    ],
+    rehypePlugins: [
+      ["rehype-highlight" as any, { strict: true, throwOnError: true }],
+      ["rehype-prism-plus", { strict: true, throwOnError: true }],
+      ["rehype-katex", { strict: true, throwOnError: true }],
+      ["rehype-slug", { strict: true, throwOnError: true }],
+    ],
+  },
+});
+
+export default withContentCollections(withMDX(nextConfig));
