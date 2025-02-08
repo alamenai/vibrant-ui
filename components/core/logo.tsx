3da0de9724527092328eaafdbcd1ccf6af79cb15
Ala <ala.menai.pro@gmail.com>
2025-02-08 18:52:36 +0100

    update: change the logo

--- components/core/logo.tsx
+++ components/core/logo.tsx
@@ -1,11 +1,13 @@
-import { Paintbrush } from "lucide-react";
-import Link from "next/link";
+import Image from "next/image"
+import Link from "next/link"
+
+import logo from "@/public/v-logo.svg"
 
 export const Logo = () => {
   return (
-    <Link href="/" className="flex items-center space-x-2 w-fit">
-      <Paintbrush />
+    <Link href="/" className="flex relative items-center gap-2">
+      <Image src={logo} width={24} height={24} alt="app logo" />
       <span className="text-nowrap">Vibrant UI</span>
     </Link>
-  );
-};
+  )
+}
