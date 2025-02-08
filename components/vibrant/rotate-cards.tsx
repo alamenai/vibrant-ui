327e1fc3451c1bd6ed9063a175cc060ec3107535
Ala <ala.menai.pro@gmail.com>
2025-02-08 18:57:37 +0100

    update: make the cards prop required in the Rotate Cards component

--- components/vibrant/rotate-cards.tsx
+++ components/vibrant/rotate-cards.tsx
@@ -13,31 +13,11 @@ export type RotateCard = {
   image: string
 }
 
-const defaultCards: RotateCard[] = [
-  {
-    name: "Dillion Verma",
-    role: "Ex. @NVIDIA,",
-    testimonial:
-      "The team's dedication to excellence and innovation truly stands out. Working with them has been an amazing journey.",
-    image: "https://avatars.githubusercontent.com/u/16860528?v=4",
-  },
-  {
-    name: "Yangshun Tay",
-    role: "Ex-Staff Eng @ Meta",
-    testimonial:
-      "Their approach to problem-solving and attention to detail made all the difference in our project's success.",
-    image: "https://avatars.githubusercontent.com/u/1315101?v=4",
-  },
-  {
-    name: "Ala Eddine Menai",
-    role: "Frontend Engineer",
-    testimonial:
-      "Outstanding service and exceptional results. They exceeded our expectations in every way possible.",
-    image: "https://avatars.githubusercontent.com/u/20143684?v=4",
-  },
-]
-
 export const RotateCards = ({ cards }: { cards: RotateCard[] }) => {
+  if (!cards || cards.length === 0) {
+    throw new Error("cards prop is missed")
+  }
+
   const [currentPosition, setCurrentPosition] = useState(0)
   const [isHovering, setIsHovering] = useState(false)
 
@@ -85,7 +65,7 @@ export const RotateCards = ({ cards }: { cards: RotateCard[] }) => {
   return (
     <div className="flex flex-col w-fit">
       <div className="flex items-center justify-center relative w-96 h-96">
-        {(cards ?? defaultCards).map((card, index) => {
+        {cards.map((card, index) => {
           const isFirst = (index - currentPosition + 3) % 3 === 0
           return (
             <motion.div
