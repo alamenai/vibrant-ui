811666f525385d3c9fdd7b9664907d5eca0e4f5f
Ala <ala.menai.pro@gmail.com>
2025-02-08 18:55:02 +0100

    update: change the style of the header

--- layouts/header.tsx
+++ layouts/header.tsx
@@ -1,13 +1,15 @@
-import { Logo } from "@/components/core/logo";
-import { Badge } from "@/components/ui/badge";
-import { Navbar } from "./navbar";
+import { Logo } from "@/components/core/logo"
+import { Badge } from "@/components/ui/badge"
+import { Navbar } from "./navbar"
 
 export const Header = () => {
   return (
-    <header className="flex py-8 px-24 space-x-2  sticky  top-0 z-50 bg-white h-20">
-      <Logo />
-      <Badge className="bg-rose-500 hover:bg-rose-500">Alpha</Badge>
+    <header className="flex py-8 px-24 space-x-2  sticky top-0 z-50 bg-white h-20">
+      <div className="flex items-center gap-2">
+        <Logo />
+        <Badge className="bg-rose-500 hover:bg-rose-500">Alpha</Badge>
+      </div>
       <Navbar />
     </header>
-  );
-};
+  )
+}
